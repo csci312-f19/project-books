d8ae5cdfc4149502e846f4ffa9f2e8161db8335d
Stephanie Jordan <sjordan@middlebury.edu>
2019-11-12 17:32:02 -0500

    showing books details

--- client/src/components/Listings.js
+++ client/src/components/Listings.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import Immutable from 'immutable';
 
 const ListingsContainer = styled.div`
   text-align: center;
@@ -30,10 +31,8 @@ const DetailedListing = ({ match }) => {
 };
 
 export function ListingsCollection({ currentListings, searchTerm }) {
-  let currCollections1 = [];
-
   const [currCollections, setColl] = useState([]);
-  const [signal, setSignal] = useState(0);
+
   if (searchTerm != null) {
     currentListings = currentListings.filter(function(listing) {
       const editedTitle = listing.Title.toUpperCase();
@@ -59,35 +58,40 @@ export function ListingsCollection({ currentListings, searchTerm }) {
         return response.json();
       })
       .then(data => {
-        // let currCollections1=currCollections;
-        currCollections1.push(data);
-        // setColl(currCollections1);
+        //use current state of collectoions
+        setColl(currState => {
+          const newstate = currState.slice();
+          data = data[0];
+          newstate.push(data);
+          return newstate;
+        });
       })
       .catch(err => console.log(err));
   };
 
-  // useEffect(()=>{
-  if (currentListings.length !== 0 && signal === 0) {
-    setSignal(1);
-    currentListings.forEach(listing => {
-      getBookByISBN(listing);
-    });
-    console.log(currCollections1);
-    setColl(currCollections1);
-  }
-  // }, [])
+  useEffect(() => {
+    if (currentListings.length !== 0) {
+      currentListings.forEach(listing => {
+        getBookByISBN(listing);
+      });
+
+      // setColl(currCollections1);
+    }
+  }, [currentListings]);
 
   console.log(currCollections);
-  let ListingsDisplay = currCollections.map(listing => (
+  let ListingsDisplay = currCollections.map(listing => {
     //Listtitle will be whatever it is that we search by
     // All the others will run though list of other properties to populate ListElement probably
 
-    <ListElementContainer key={listing.ISBN}>
-      <ListTitle>{listing.title}</ListTitle>
-      <ListElement>{listing.courseID}</ListElement>
-      <Link to={listing.ISBN}>More Info</Link>
-    </ListElementContainer>
-  ));
+    return (
+      <ListElementContainer key={listing.ISBN}>
+        <ListTitle>{listing.title}</ListTitle>
+        <ListElement>{listing.courseID}</ListElement>
+        <Link to={listing.ISBN}>More Info</Link>
+      </ListElementContainer>
+    );
+  });
   // <ListElementContainer key={listing.ISBN}>
   //   <ListTitle>{listing.Title}</ListTitle>
   //   <ListElement>{listing.courseID}</ListElement>
@@ -97,6 +101,7 @@ export function ListingsCollection({ currentListings, searchTerm }) {
   //   <ListElement>{listing.condition}</ListElement>
   //   <Link to={listing.ISBN}>More Info</Link>
   // </ListElementContainer>
+
   return (
     <ListingsContainer>
       <List>{ListingsDisplay}</List>
