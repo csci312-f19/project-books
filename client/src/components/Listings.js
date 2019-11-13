7c416d894b80c7a96f4754d4b53c33a82b787a27
Grant Berman <grantberman1@live.com>
2019-11-12 21:37:32 -0500

    updated variable names

--- client/src/components/Listings.js
+++ client/src/components/Listings.js
@@ -47,28 +47,11 @@ const DetailedListing = () => {
   );
 };
 
-function getBookByISBN(listing) {
-  // alert("in call")
-
-  const isbn = listing.ISBN;
-  fetch(`/api/books/${isbn}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-    })
-    .then(data => {
-      return data; //how do i return this data at the end of the function ? let will not persist outside of scope
-    })
-    .catch(err => console.log(err));
-}
-
 export function ListingsCollection({ currentListings, searchTerm }) {
   if (searchTerm != null) {
     currentListings = currentListings.filter(function(listing) {
-      const editedTitle = listing.Title.toUpperCase();
-      const editedCourseTitle = listing.courseTitle.toUpperCase();
+      const editedTitle = listing.title.toUpperCase();
+      const editedCourseTitle = listing.courseID.toUpperCase();
       // let editedAuthor=listing.Author.toUpperCase();
 
       return (
@@ -78,15 +61,10 @@ export function ListingsCollection({ currentListings, searchTerm }) {
       );
     });
   }
-  if (currentListings.length !== 0) {
-    console.log(currentListings);
-    const book = getBookByISBN(currentListings[0]); //change this to only take in an ID ?
-    console.log(book);
-    // console.log("^^^")
-  }
 
+  console.log(currentListings);
   const ListingsDisplay = currentListings.map(listing => (
-    //Listtitle will be whatever it is that we search by
+    // Listtitle will be whatever it is that we search by
     // All the others will run though list of other properties to populate ListElement probably
 
     <ListElementContainer key={listing.ISBN}>
