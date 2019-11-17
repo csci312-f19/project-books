5e711e1388f7a7b07b5fb61c54ad7fd95089e933
Grant Berman <grantberman1@live.com>
2019-11-17 17:14:04 -0500

    prod db config

---
+++ server/seeds/load-transactions.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const fs = require('fs');
+
+exports.seed = knex => {
+  const contents = fs.readFileSync(
+    path.join(__dirname, '/DB_data/transactionsSampleData.json')
+  );
+  const data = JSON.parse(contents);
+  // Deletes ALL existing entries
+  return knex('Transactions')
+    .del()
+    .then(() => knex.batchInsert('Transactions', data, 100));
+};
