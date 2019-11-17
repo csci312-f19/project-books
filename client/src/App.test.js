5bf20d12b7ed01cf472e9b8c7af9b1564e2b97fd
Grant Berman <grantberman1@live.com>
2019-11-17 14:50:29 -0500

    configured test db and wrote some client and app side tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import SearchBar from './components/SearchBar';
+import Listings from './components/Listings';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Immutable from 'immutable';
+import { act } from 'react-dom/test-utils';
+import { mount } from 'enzyme';
+import { pseudoServer, flushPromises } from './setupTests';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('App rendering tests', () => {
+  let app;
+
+  beforeEach(async () => {
+    pseudoServer.initialize();
+    app = mount(<App />);
+    await act(async () => await flushPromises());
+    app.update();
+  });
+
+  describe('App component initial content', () => {
+    test('Contains an SearchBar component', () => {
+      expect(app).toContainExactlyOneMatchingElement(SearchBar);
+    });
+  });
+
+  describe('App component initial content', () => {
+    test('Contains an listings component', () => {
+      expect(app).toContainExactlyOneMatchingElement(Listings);
+    });
+  });
 });
