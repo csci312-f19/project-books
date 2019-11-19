bb3ecf6f5bcfd5b5235cc46eab52903d7feddbc0
AnnikaXu <xinyuex@middlebury.edu>
2019-11-18 21:33:09 -0500

    user Account done

--- client/src/components/sortBar.test.js
+++ client/src/components/sortBar.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { mount } from 'enzyme';
 import Immutable from 'immutable';
 import { SortBar } from './Listings';
 import data from '../Data/SampleData.json';
