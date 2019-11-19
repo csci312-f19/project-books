d860b77f5321949374fd57eb807437c5cc2eab30
Unknown <noleary82@gmail.com>
2019-11-19 13:03:43 -0500

    trying googleclient again

--- client/src/App.js
+++ client/src/App.js
@@ -8,14 +8,23 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Immutable from 'immutable';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
-const GOOGLE_CLIENT_ID = () => {
-  if (window.location.href.contains('localhost')) {
-    return '603582413711-motmdqbcfj8drljfjisq4ihtobolfemt.apps.googleusercontent.com';
+const setGoogleID = () => {
+  if (window.location.href === 'localhost') {
+    console.log('in if');
+    const GOOGLE_CLIENT_ID =
+      '603582413711-motmdqbcfj8drljfjisq4ihtobolfemt.apps.googleusercontent.com';
+    return GOOGLE_CLIENT_ID;
   } else {
     // in Heroku
-    return '304836268474-hetmurq1ojali48o345mp1k9atjo74ss.apps.googleusercontent.com';
+    console.log('in else');
+    const GOOGLE_CLIENT_ID =
+      '304836268474-hetmurq1ojali48o345mp1k9atjo74ss.apps.googleusercontent.com';
+    return GOOGLE_CLIENT_ID;
   }
 };
+
+const GOOGLE_CLIENT_ID = setGoogleID();
+console.log(GOOGLE_CLIENT_ID);
 
 //import Immutable from 'immutable';   // need to do "npm install immutable
 
