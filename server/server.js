071fce19c3c6b3f67846290d61cf9044114ffa8f
Grant Berman <grantberman1@live.com>
2019-12-06 13:15:14 -0500

    linting issue?

--- server/server.js
+++ server/server.js
@@ -259,14 +259,6 @@ app.get('/api/users', (request, response, next) => {
     }, next); // <- Notice the "next" function as the rejection handler
 });
 
-app.get(`/api/googleID/:id`, (request, response, next) => {
-  User.query()
-    .where('id', request.params.id)
-    .then(rows => {
-      response.send(rows);
-    }, next); // <- Notice the "next" function as the rejection handler
-});
-
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
   // All remaining requests return the React app, so it can handle routing.
